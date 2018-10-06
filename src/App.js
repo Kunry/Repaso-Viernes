8d625409d3ac7d8daabccae72d9cfcc54002c5da
Kunry <kunry3@hotmail.com>
2018-10-07 01:40:33 +0200

    limpiado el código

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import _ from "lodash";
 
 import { FilterData } from "./Component/FilterData";
 import {Search} from "./Component/Search";
@@ -19,22 +18,22 @@ export default class App extends Component {
   /* Dividimos la busqueda y el checkbox para poder hacer que los dos funcionen correctamente y filtren ambas. */
 
   Search(search, checkbox) {
-    let _data = search;
-    _data = data.filter(e => {      // Para hacer un mejor filtro siempre es bueno utilizar Expresiones Regulares
-        const expReg = new RegExp(_data, "ig") // Se declara RegExp con el valor del input, se le dice que no diferencie entre mayusculas y minusculas (i), y que busque en todo el string (g)
+    let newdata = search;
+    newdata = data.filter(e => {      // Para hacer un mejor filtro siempre es bueno utilizar Expresiones Regulares
+        const expReg = new RegExp(newdata, "ig") // Se declara RegExp con el valor del input, se le dice que no diferencie entre mayusculas y minusculas (i), y que busque en todo el string (g)
         return expReg.test(e.name) // se utiliza la propiedad test() que nos devuelve true o false si encuentra algún valor
     })  
-    // _data = data.filter(e => e.name.includes(_data)); // Con esta tambien os vale, pera para que veáis otra forma de hacer.
-    this.setState({ data: _data, search }, () => {
+    // newdata = data.filter(e => e.name.includes(newdata)); // Con esta tambien os vale, pera para que veáis otra forma de hacer.
+    this.setState({ data: newdata, search }, () => {
       this.Checkbox(checkbox);
     });
   }
   // Está es la función que hace el filtro del checkbox
   Checkbox(checkbox) {
-    const _data = this.state.data.filter(
+    const newdata = this.state.data.filter(
       e => (!checkbox ? true : e.stocked == checkbox)
     );
-    this.setState({ data: _data, checkbox });
+    this.setState({ data: newdata, checkbox });
   }
 
   // Decimos que datos tienen que ir con cada uno, si lo que recibimos es un string el valor del input es de text y le damos el valor por defecto al checkbox, sino lo hacemos al contrario.
